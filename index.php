a155c1342a18684ffd3ce8dc7bb9147c63d80407
ferbondioni <fernando@gravityonmars.com>
2011-04-07 16:00:06 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php include('header.php') ?>
+
+		<div data-role="content" class="content">
+			
+			<div style="text-align: center">
+				<img src="images/logo.png" style="margin: auto; margin-bottom: 3%; margin-top: -10%; width: 60%" alt="Logo">
+			</div>
+			
+			<div class="ui-grid-b">
+				<div class="ui-block-a">Block A</div>
+				<div class="ui-block-b">Block B</div>
+				<div class="ui-block-c">Block C</div>
+			</div><!-- /grid-a -->		
+			
+			<div class="menu rounded">
+				<a href="projects.html" class="border-right">Projects</a>
+				<a href="#">Services</a>
+				<a href="#" class="border-right">About us</a>
+				<a href="#">Notepad</a>
+				<a href="#" class="border-right">Contact Us</a>
+				<a href="http://twitter.com/gravityonmars" target="_blank">Follow Us</a>
+			</div>
+			
+			<h3>Company Updates</h3>
+			<div id="" class="latest-news rounded">
+				<a href="" class="entry">
+					<span class="entry-date">3h ago</span>
+					<span class="entry-description">
+						New Title
+					</span>
+				</a>
+				
+				<a href="" class="entry">
+					<span class="entry-date">12h ago</span>
+					<span class="entry-description">
+						New Title
+					</span>
+				</a>
+				
+				<a href="" class="entry">
+					<span class="entry-date">14h ago</span>
+					<span class="entry-description">
+						New Title
+					</span>
+				</a>
+				
+				<a href="" class="entry">
+					<span class="entry-date">14h ago</span>
+					<span class="entry-description">
+						New Title
+					</span>
+				</a>
+			</div>
+			
+		</div> 
+
+<?php include('footer.php') ?>
